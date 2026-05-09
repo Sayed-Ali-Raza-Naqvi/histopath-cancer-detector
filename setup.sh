2b6df29ebea4352c2987c509e9921a912157256b
Syed Ali Raza Naqvi <alisyednaqvi1512@gmail.com>
2026-05-09 09:44:47 +0500

    test.py added

--- setup.sh
+++ setup.sh
@@ -3,7 +3,7 @@
 mkdir -p {api,model,app/pages,data,assets}
 
 touch api/{main.py,inference.py,gradcam.py}
-touch model/{train.py,evaluate.py,dataset.py}
+touch model/{train.py,evaluate.py,dataset.py,test.py}
 touch app/{streamlit_app.py,pages/model_info.py}
 touch {Dockerfile,docker-compose.yml,requirements.txt,README.md,.gitignore}
 
